1045682723e1712b553caf5c00553025e15ea027
SitoNumbis <sito8943@gmail.com>
2022-01-30 22:31:16 -0500

    done, using prompt

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
+const prompt = require("prompt");
+
 let input = 0;
+
+// letters
+letters = [
+  "T",
+  "R",
+  "W",
+  "A",
+  "G",
+  "M",
+  "Y",
+  "F",
+  "P",
+  "D",
+  "X",
+  "B",
+  "N",
+  "J",
+  "Z",
+  "S",
+  "Q",
+  "V",
+  "H",
+  "L",
+  "C",
+  "K",
+  "E",
+];
+
 // if exist argv 2
 if (process.argv[2]) input = process.argv[2];
-else {
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: proces.stdout,
-  });
 
-  rl.question("Waiting for input: ", (numb) => {
-    input = numb;
-    rl.close();
-  });
-}
+const properties = [
+  {
+    name: "input",
+    validator: /[0-9]/,
+    warning: "Number must be only numbers",
+  },
+];
 
-const DNI = String(input) + String(input % 23);
-console.log("Your DNI is: ", DNI);
+prompt.start();
 
-process.exit(0);
+prompt.get(properties, function (err, result) {
+  if (err) {
+    return onErr(err);
+  }
+  input = result.input;
+
+  console.log("Command-line input received:");
+
+  const DNI = `${input}-${letters[input % 23]}`;
+
+  console.log("Your DNI is: ", DNI);
+  process.exit(0);
+});
+
+onErr = (err) => {
+  console.log(err);
+  return 1;
+};
